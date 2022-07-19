300ac63504c631ff860ea2a4d237c4de65333eaa
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-07-19 21:23:56 +0300

    js to input data on innerHTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,18 @@ function totalProduction(){
     var shedDTotal = document.getElementsByName("shedDTotal")[0].ariaValueMax;
     let total =Number(shedATotal)+ Number(shedBTotal) +Number(shedCTotal) + Number(shedDTotal);
 
+    
+    var shedATotal='your production for shed A is'+''+shedATotal+''+ 'per day';
+    var shedBTotal='your production for shed A is'+''+shedBTotal+''+ 'per day';
+    var shedCTotal='your production for shed A is'+''+shedCTotal+''+ 'per day';
+    var shedDTotal='your production for shed A is'+''+shedDTotal+''+ 'per day';
+
+    document.getElementByName("shedA-production")[0].innerHTML=shedATotal;
+    document.getElementByName("shedB-production")[0].innerHTML=shedBTotal;
+    document.getElementByName("shedC-production")[0].innerHTML=shedCTotal;
+    document.getElementByName("shedD-production")[0].innerHTML=shedDTotal;
     document.getElementByName("sum")[0].value=total;
+    total
+
     
 }
