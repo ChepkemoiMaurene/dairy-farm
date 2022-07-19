49c7fddedfcbe62b8763d4af5c31406607f9f38f
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-07-19 22:03:11 +0300

    js editting to calculate total production

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 function totalProduction(){
-    var shedATotal = document.getElementsByName("shedATotal")[0].ariaValueMax;
-    var shedBTotal = document.getElementsByName("shedBTotal")[0].ariaValueMax;
-    var shedCTotal = document.getElementsByName("shedCTotal")[0].ariaValueMax;
-    var shedDTotal = document.getElementsByName("shedDTotal")[0].ariaValueMax;
+    var shedATotal = document.getElementsByName("shedATotal")[0].value;
+    var shedBTotal = document.getElementsByName("shedBTotal")[0].value;
+    var shedCTotal = document.getElementsByName("shedCTotal")[0].value;
+    var shedDTotal = document.getElementsByName("shedDTotal")[0].value;
     let total =Number(shedATotal)+ Number(shedBTotal) +Number(shedCTotal) + Number(shedDTotal);
 
-    
-    var shedATotal='your production for shed A is'+''+shedATotal+''+ 'per day';
-    var shedBTotal='your production for shed A is'+''+shedBTotal+''+ 'per day';
-    var shedCTotal='your production for shed A is'+''+shedCTotal+''+ 'per day';
-    var shedDTotal='your production for shed A is'+''+shedDTotal+''+ 'per day';
+    document.getElementsByName("sum")[0].value=total;
 
-    document.getElementByName("shedA-production")[0].innerHTML=shedATotal;
-    document.getElementByName("shedB-production")[0].innerHTML=shedBTotal;
-    document.getElementByName("shedC-production")[0].innerHTML=shedCTotal;
-    document.getElementByName("shedD-production")[0].innerHTML=shedDTotal;
-    document.getElementByName("sum")[0].value=total;
-    total
+    var shedAProduction='your production for shed A is'+""+shedATotal+""+ 'per day';
+    var shedBProduction='your production for shed A is'+''+shedBTotal+''+ 'per day';
+    var shedAProduction='your production for shed A is'+''+shedCTotal+''+ 'per day';
+    var shedDProduction='your production for shed A is'+''+shedDTotal+''+ 'per day';
 
+    document.getElementsByClassName("shedA-production")[0].innerHTML=shedAProduction;
+    document.getElementsByClassName("shedB-production")[0].innerHTML=shedBProduction;
+    document.getElementsByClassName("shedC-production")[0].innerHTML=shedAProduction;
+    document.getElementsByClassName("shedD-production")[0].innerHTML=shedDProduction;
     
+    totalShedProduction='you production of all the shed is'+''+total+''+ 'per day';
+
+    document.getElementsByID("total-production")[0].innerHTML=totalShedProduction;
+
 }
+totalProduction();
