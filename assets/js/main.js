0de21ba8f80e17790e69acaecba93cea3fa7297a
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-07-19 23:12:50 +0300

    js editting

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,13 @@ function totalProduction(){
     document.getElementsByName("sum")[0].value=total;
 
     var shedAProduction='your production for shed A is'+""+shedATotal+""+ 'per day';
-    var shedBProduction='your production for shed A is'+''+shedBTotal+''+ 'per day';
-    var shedAProduction='your production for shed A is'+''+shedCTotal+''+ 'per day';
-    var shedDProduction='your production for shed A is'+''+shedDTotal+''+ 'per day';
+    var shedBProduction='your production for shed B is'+''+shedBTotal+''+ 'per day';
+    var shedCProduction='your production for shed C is'+''+shedCTotal+''+ 'per day';
+    var shedDProduction='your production for shed D is'+''+shedDTotal+''+ 'per day';
 
     document.getElementsByClassName("shedA-production")[0].innerHTML=shedAProduction;
     document.getElementsByClassName("shedB-production")[0].innerHTML=shedBProduction;
-    document.getElementsByClassName("shedC-production")[0].innerHTML=shedAProduction;
+    document.getElementsByClassName("shedC-production")[0].innerHTML=shedCProduction;
     document.getElementsByClassName("shedD-production")[0].innerHTML=shedDProduction;
     
     totalShedProduction='you production of all the shed is'+''+total+''+ 'per day';
@@ -23,3 +23,6 @@ function totalProduction(){
 
 }
 totalProduction();
+*45
+
+let milk = (100 + 50) * 45;
