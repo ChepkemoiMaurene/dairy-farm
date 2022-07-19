59cef5b4ba1cd563c373b71c3b97f42c3a0dab8f
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-07-19 21:07:11 +0300

    initiallizing js for dailly production

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function totalProduction(){
+    var shedATotal = document.getElementsByName("shedATotal")[0].ariaValueMax;
+    var shedBTotal = document.getElementsByName("shedBTotal")[0].ariaValueMax;
+    var shedCTotal = document.getElementsByName("shedCTotal")[0].ariaValueMax;
+    var shedDTotal = document.getElementsByName("shedDTotal")[0].ariaValueMax;
+    
+}
